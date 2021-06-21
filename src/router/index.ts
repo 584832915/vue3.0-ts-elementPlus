ab9ea8a83914d34cc700cba0cbb652874aa0eb0f
584832915 <584832915@qq.com>
2021-06-21 18:41:25 +0800

    动态路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { handleRoutes } from './../utils/handlePermission';
 import store from '@/store'
-import { createRouter, createWebHashHistory, RouteRecordRaw, RouteLocationNormalized } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw, RouteLocationNormalized,NavigationGuardNext } from 'vue-router'
 import Login from '../views/login.vue'
 
 
@@ -21,23 +21,25 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
-const canUserAccess = (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
+const canUserAccess = (to: RouteLocationNormalized, from: RouteLocationNormalized,next:NavigationGuardNext) => {
   if (to.path == '/') {
     return true;
   } else {
     if (sessionStorage.getItem('token')) {
       if(!store.state.permission.sidbarList.length){
         handleRoutes()
+        next({ ...to, replace: true })
+      }else{
+        next()
       }
-      return true;
     } else {
       return !to.meta.parent ? true : router.push({ path: '/' })
     }
   }
 }
-router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
+router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormalized,next:NavigationGuardNext) => {
  
-  return await canUserAccess(to, from)
+  return await canUserAccess(to, from ,next)
 })
 
 export default router
